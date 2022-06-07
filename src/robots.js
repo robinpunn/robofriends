8710ccd53cf6bf1ce49cfdbc7471b1177538a0fc
Robin Punnoose <robin.punnoose@protonmail.com>
2022-06-07 12:19:56 -0500

    adding initial files

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Adalwin Jaxtyn',
+    username: 'AdaJax',
+    email: 'adalwin@jaxtyn.biz'
+  },
+  {
+    id: 2,
+    name: 'Kristina Maarika',
+    username: 'Kris',
+    email: 'kristina@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Sumit Odoacer',
+    username: 'Sumi',
+    email: 'sumoda@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Kirill Antonina',
+    username: 'Kirill',
+    email: 'kirill.antonina@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Zhaleh Dóra',
+    username: 'Zha',
+    email: 'zha_zha@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Moerani Yehiel',
+    username: 'Moe',
+    email: 'moerani_yehiel@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Zejd Olindo',
+    username: 'ZO',
+    email: 'zejdolindo@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Gotthilf Shehu',
+    username: 'Gotthi',
+    email: 'Shehu@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Cemal Honza',
+    username: 'CeHon',
+    email: 'Cem_Hon@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Soo-Jin Jere',
+    username: 'Soo.Jin',
+    email: 'SooJJ@karina.biz'
+  }
+];
